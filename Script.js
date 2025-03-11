f6bd872fdf21ddea7fd95ce86a86a98e069ad116
Ankit <152708564+ANKIT-stack-glitch@users.noreply.github.com>
2025-03-12 00:00:53 +0545

    display team stats on page

--- Script.js
+++ Script.js
@@ -6,3 +6,8 @@ const teamStats = {
   year: 2025,
   headCoach: "Abdullah Almutairi",
 };
+
+document.getElementById("team").textContent = teamStats.team;
+document.getElementById("sport").textContent = teamStats.sport;
+document.getElementById("year").textContent = teamStats.year;
+document.getElementById("head-coach").textContent = teamStats.headCoach;
