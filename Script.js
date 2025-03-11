211a25dbf44355e619af34d83657d9427e1d0998
Ankit <152708564+ANKIT-stack-glitch@users.noreply.github.com>
2025-03-12 00:00:09 +0545

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,2 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", () => {});
+const teamStats = {
+  team: "Nepal National Football Team",
+  sport: "Football",
+  year: 2025,
+  headCoach: "Abdullah Almutairi",
+};
